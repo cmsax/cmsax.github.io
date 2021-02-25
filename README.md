012a0cbd92a6baa17d8019164cf98daf172a3607
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-02-25 02:53:45 +0000

    Deploy cmsax/cmsax.github.io to github.com/cmsax/cmsax.github.io.git:master

---
+++ README.md
@@ -0,0 +1,11 @@
+# Homepage
+
+[![Build Status](https://travis-ci.com/cmsax/cmsax.github.io.svg?branch=master)](https://travis-ci.com/cmsax/cmsax.github.io)
+
+[View this page](https://www.unoiou.com).
+
+Source code is available on `react` branch, `master` is for deployment.
+
+## Copyright
+
+Copyright (c) 2019 Cai Mingshi. All rights reserved.
---
+++ precache-manifest.5c367a4cef35bb6c60676e6f10c1599d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeee0bae0761f23e58b1176c36a255c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3bb43fb3606c9b178d7",
+    "url": "/static/css/main.73b606c3.chunk.css"
+  },
+  {
+    "revision": "f13cdb52a464b0cff38d",
+    "url": "/static/js/2.f4eea159.chunk.js"
+  },
+  {
+    "revision": "e3bb43fb3606c9b178d7",
+    "url": "/static/js/main.b9634aaa.chunk.js"
+  },
+  {
+    "revision": "b12c66a0f6a4f7394103",
+    "url": "/static/js/runtime-main.d7aebda3.js"
+  }
+]);
